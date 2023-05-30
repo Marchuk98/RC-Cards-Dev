dd7b211762beec4a95f5f209625803227c7d0f3e
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-05-30 15:05:15 +0300

    popup error injection

---
+++ src/common/snackbar/Snackbar.tsx
@@ -0,0 +1,36 @@
+// import { forwardRef, SyntheticEvent } from 'react'
+//
+// import MuiAlert, { AlertProps } from '@mui/material/Alert'
+// import Snackbar from '@mui/material/Snackbar'
+//
+// import { appActions } from 'app/app-slice'
+// import { useAppData } from 'app/use-app-data'
+//
+// const Alert = forwardRef<HTMLDivElement, AlertProps>((props, ref) => {
+//     return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />
+// })
+//
+// export const DescriptionSnackbar = () => {
+//     const { error, infoMessage } = useAppData()
+//     const dispatch = useAppDispatch()
+//
+//     const handleClose = (event?: SyntheticEvent | Event, reason?: string) => {
+//         if (reason === 'clickaway') {
+//             return
+//         }
+//
+//         dispatch(appActions.resetInfoMessages())
+//     }
+//
+//     return (
+//         <Snackbar open={!!error || !!infoMessage} autoHideDuration={5000} onClose={handleClose}>
+//             <Alert
+//                 onClose={handleClose}
+//                 severity={infoMessage ? 'success' : 'error'}
+//                 sx={{ width: '100%' }}
+//             >
+//                 {infoMessage || error}
+//             </Alert>
+//         </Snackbar>
+//     )
+// }
