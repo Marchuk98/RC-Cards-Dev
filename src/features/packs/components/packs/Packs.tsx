763f39c4a1ec7bb973989a3ad65e8abbcd1e337f
Yelkira <penda.fu@gmail.com>
2023-06-07 23:32:41 +0300

    feature/ just to render need to refactor(add inputSearch)

--- src/features/packs/components/packs/Packs.tsx
+++ src/features/packs/components/packs/Packs.tsx
@@ -1,7 +1,20 @@
+import {useEffect} from "react";
+import {useAppDispatch, useAppSelector} from "../../../../app/hooks.ts";
+import {InputSearch} from "../../../../common/components/InputSearch/InputSearch.tsx";
 import {PackTable} from "../packTable/PackTable.tsx";
+import {getPacks, packListActions} from "./pack-listSlice.ts";
 
 export const Packs = () => {
+  const dispatch = useAppDispatch()
+  const packName = useAppSelector(state => state.packListReducer.queryParams.packName)
+  const onSearchChange = (search: string) => {
+    dispatch(packListActions.setQueryParams({ packName: search }))
+  }
+  useEffect(() => {
+    dispatch(getPacks())
+  }, [packName])
   return <div>
+    <InputSearch onChangeValue={onSearchChange} searchValue={packName} />
     <PackTable/>
   </div>;
 };
