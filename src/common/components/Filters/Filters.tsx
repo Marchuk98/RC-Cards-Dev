afab5f380547579e4624563219696c2b1c1525ed
Yelkira <penda.fu@gmail.com>
2023-06-08 18:48:58 +0300

    feature/ add FilterPanels and Filters

---
+++ src/common/components/Filters/Filters.tsx
@@ -0,0 +1,16 @@
+import { FC, PropsWithChildren } from 'react'
+
+import Grid from '@mui/material/Grid'
+
+export const Filters: FC<PropsWithChildren> = ({ children }) => {
+    return (
+        <Grid
+            container
+            justifyContent={'space-between'}
+            alignItems={'center'}
+            sx={{ paddingTop: 3, marginBottom: 3 }}
+        >
+            {children}
+        </Grid>
+    )
+}
---
+++ src/features/packs/components/FilterPanels/FilterPanels.tsx
@@ -0,0 +1,71 @@
+import {useEffect} from "react";
+import {useParams} from "react-router-dom";
+import {useAppDispatch, useAppSelector} from "../../../../app/hooks.ts";
+import {ButtonsGroup} from "../../../../common/components/ButtonsGroup/ButtonsGroup.tsx";
+import {CustomSlider} from "../../../../common/components/CustomSlider/CustomSlider.tsx";
+import {Filters} from "../../../../common/components/Filters/Filters.tsx";
+import {InputSearch} from "../../../../common/components/InputSearch/InputSearch.tsx";
+import {Reset} from "../../../../common/components/ResetButton/Reset.tsx";
+import {
+    maxParams,
+    minParams,
+    packListMaxCardsCount,
+    packListMinCardsCount, packListPage, packListPageCount,
+    packListStatus,
+    packNameParams, userIdParams
+} from "../../selectors.ts";
+import {getPacks, packActions} from "../packs/pack-listSlice.ts";
+
+export const FilterPanels = () => {
+    const dispatch = useAppDispatch()
+    const {packId} = useParams<{ packId: string }>()
+    const packName = useAppSelector(packNameParams)
+    const minCardsCount = useAppSelector(packListMinCardsCount)
+    const maxCardsCount = useAppSelector(packListMaxCardsCount)
+    const min = useAppSelector(minParams)
+    const max = useAppSelector(maxParams)
+    const status = useAppSelector(packListStatus)
+    const {_id: userId} = useAppSelector(state => state.profileReducer.profile)
+    const pageCount = useAppSelector(packListPageCount)
+    const page = useAppSelector(packListPage)
+    const user_id = useAppSelector(userIdParams)
+    useEffect(() => {
+        dispatch(getPacks({cardsPack_id: packId as string}))
+    }, [packName, max, min, user_id])
+    const onSearchChange = (search: string) => {
+        dispatch(packActions.setQueryParams({packName: search}))
+    }
+    const onChangeSliderValue = (event: any, numbers: number[] | number) => {
+        if (Array.isArray(numbers)) {
+            dispatch(packActions.setQueryParams({ min: numbers[0], max: numbers[1] }))
+        }
+    }
+    const getMyPacks = () => {
+        dispatch(
+            packActions.setQueryParams({ min, max, user_id: userId, page: page, pageCount: pageCount })
+        )
+    }
+    const getAllPacks = () => {
+        dispatch(packActions.setQueryParams({ user_id: '' }))
+    }
+    const onReset = () => {
+        dispatch(packActions.resetQueryParams())
+    }
+    return (
+        <Filters>
+            <InputSearch onChangeValue={onSearchChange} searchValue={packName}/>
+            <ButtonsGroup
+                disabled={status === 'loading'}
+                onClickMy={getMyPacks}
+                onClickAll={getAllPacks}
+            />
+            <CustomSlider
+                disabled={status === 'loading'}
+                minMax={[min, max]}
+                onChangeCommitted={onChangeSliderValue}
+                values={[minCardsCount, maxCardsCount]}
+            />
+            <Reset disabled={status === 'loading'} onIconClick={onReset} />
+        </Filters>
+    )
+}
