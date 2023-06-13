52c90ecc9d74c52deba76675fe94793cf223d273
Yelkira <penda.fu@gmail.com>
2023-06-13 23:28:35 +0300

    feature/ add Question component

---
+++ src/features/learn/components/Question/Question.tsx
@@ -0,0 +1,25 @@
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
+import {useLearn} from "../../hooks/useLearn.ts";
+
+
+export const Question = () => {
+    const { card } = useLearn()
+    return (
+        <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+            <Typography
+                sx={{ marginBottom: 1.5, display: 'flex', flexDirection: 'row', alignItems: 'center' }}
+            >
+                <b>Question:&nbsp;</b>
+                {card.questionImg!=="no_image"&&card.questionImg!==""&&card.questionImg!==undefined ? (
+                    <img style={{ height: '35px', marginLeft: '20px' }} alt="img" src={card.questionImg} />
+                ) : (
+                    card.question
+                )}
+            </Typography>
+            <Typography sx={{ opacity: 0.5, marginBottom: 3.7 }}>
+                Number of answers to the question:&nbsp;<b>{card.shots}</b>
+            </Typography>
+        </Box>
+    )
+}
