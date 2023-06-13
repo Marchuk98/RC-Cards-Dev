eaef866d7aec2816a3cab8b72f1cd1408b21e3d0
Yelkira <penda.fu@gmail.com>
2023-06-13 23:24:21 +0300

    feature/ add AnswerForm component

---
+++ src/features/learn/components/Answer/AnswerForm.tsx
@@ -0,0 +1,38 @@
+import Button from '@mui/material/Button'
+import FormControlLabel from '@mui/material/FormControlLabel'
+import FormLabel from '@mui/material/FormLabel'
+import Radio from '@mui/material/Radio'
+import RadioGroup from '@mui/material/RadioGroup'
+import { useForm } from 'react-hook-form'
+import {UpdateGradeRequestType} from "../../../cards/components/cards/types.ts";
+import {useLearn} from "../../hooks/useLearn.ts";
+
+
+const grades = ['Did not know', 'Forgot', 'A lot of thought', 'Confused', 'Knew the answer']
+
+export const AnswerForm = () => {
+    const { onSubmit } = useLearn()
+
+    const { handleSubmit, register } = useForm<UpdateGradeRequestType>()
+
+    const radioItems = grades.map((grade, index) => (
+        <FormControlLabel
+            key={index}
+            value={index + 1}
+            control={<Radio {...register('grade')} />}
+            label={grade}
+        />
+    ))
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <FormLabel>Rate yourself:</FormLabel>
+            <RadioGroup sx={{ m: '0 0 42px 0' }} defaultValue="1">
+                {radioItems}
+            </RadioGroup>
+            <Button sx={{ width: '100%' }} size={'large'} variant={'contained'} type={'submit'}>
+                Next
+            </Button>
+        </form>
+    )
+}
