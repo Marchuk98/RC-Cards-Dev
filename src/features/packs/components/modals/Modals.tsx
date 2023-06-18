325a8e2c69983c2ba042893757c856ffc5f55066
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-06-18 23:50:54 +0300

    A component that allows the rendering of modal windows for deleting, adding and editing

---
+++ src/features/packs/components/modals/Modals.tsx
@@ -0,0 +1,43 @@
+import {useModals} from "../../../modals/hooks/useModals.ts";
+import {usePackList} from "../../hooks/usePackList.ts";
+import {DeleteModal} from "../../../../common/components/Modal/DeleteModal/DeleteModal.tsx";
+import {PackListCommonModal} from "./PackCommonModal.tsx";
+
+
+export const Modals = () => {
+
+    const {isAddedModalVisible,
+           isEditModalVisible,
+           isDeleteModalVisible,
+        closeModal,
+    data:{_id,name,question,deckCover}
+    } = useModals()
+
+    const {addNewPack,editPack,removePack,status} = usePackList()
+
+    return (
+        <>
+            <DeleteModal
+                open={isDeleteModalVisible}
+                modalTitle={'Do you want to remove'}
+                handleClose={closeModal}
+                disabled={status === 'loading'}
+                entityTitle={'Pack'}
+                entityName={name || question}
+                callBack={removePack(_id)}
+            />
+            <PackListCommonModal
+                open={isAddedModalVisible}
+                title={'Add Pack'}
+                status={status === 'loading'}
+                callback={addNewPack(deckCover)}
+                />
+            <PackListCommonModal
+                open={isEditModalVisible}
+                title={'Edit Pack'}
+                status={status === 'loading'}
+                callback={editPack(_id,deckCover)}
+            />
+        </>
+    )
+}
