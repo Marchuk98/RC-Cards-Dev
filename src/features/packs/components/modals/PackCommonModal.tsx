cc3bd0ec71e0f2a8095f430ed9c17cdeffaafcdb
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-06-18 23:51:40 +0300

    Universal component for adding and editing

---
+++ src/features/packs/components/modals/PackCommonModal.tsx
@@ -0,0 +1,35 @@
+import {memo} from "react";
+import {useModals} from "../../../modals/hooks/useModals.ts";
+import {BasicModal} from "../../../../common/components/Modal/BasicModal/BasicModal.tsx";
+import {ModalsForms} from "../../../../common/components/Forms/ModalsForms.tsx";
+
+
+type EditModalType = {
+    open: boolean
+    title: string
+    status: boolean
+    callback: (data: any) => void
+}
+
+export const PackListCommonModal= memo(({ callback, open, title, status }:EditModalType) => {
+    const {
+        closeModal,
+        setDeckCover,
+        removeImages,
+        data: { name, deckCover },
+    } = useModals()
+
+    return (
+        <BasicModal open={open} onClose={closeModal} title={title}>
+            <ModalsForms
+                name={name}
+                disabled={status}
+                deckCover={deckCover}
+                callback={callback}
+                closeModal={closeModal}
+                setDeckCover={setDeckCover}
+                removeCover={removeImages}
+            />
+        </BasicModal>
+    )
+})
