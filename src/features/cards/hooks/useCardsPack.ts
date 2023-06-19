8020c97555904b63753b8776e27d09a11c1daa6c
Yelkira <penda.fu@gmail.com>
2023-06-19 20:27:19 +0300

    feature/ add selectors and some changes

--- src/features/cards/hooks/useCardsPack.ts
+++ src/features/cards/hooks/useCardsPack.ts
@@ -1,6 +1,14 @@
+import {useNavigate, useParams} from "react-router-dom";
 import {useAppSelector} from "../../../app/hooks.ts";
 import {useProfile} from "../../../app/hooks/useProfile/useProfile.ts";
-import {cardPackCardItem, cardPackUserId, cardPageCount, cardStatus} from "../components/cards/selectors.ts";
+import {packCardPacksName} from "../components/cards/cardSelectors.ts";
+import {
+    cardPackCardItem,
+    cardPackUserId,
+    cardPageCount,
+    cardStatus,
+    cardsTotalCount, packDeckCover
+} from "../components/cards/selectors.ts";
 
 
 export const useCardsPack = () => {
@@ -8,15 +16,28 @@ export const useCardsPack = () => {
     const cardPacks = useAppSelector(cardPackCardItem)
     const pageCount = useAppSelector(cardPageCount)
     const status = useAppSelector(cardStatus)
-    const {_id} = useProfile()
     const cardUserId = useAppSelector(cardPackUserId)
-    const isMe = cardUserId === _id
+    const cardName = useAppSelector(packCardPacksName)
+    const totalCount = useAppSelector(cardsTotalCount)
+    const cover = useAppSelector(packDeckCover)
 
+    const {_id} = useProfile()
+    const isMe = cardUserId === _id
+    const navigate = useNavigate()
+    const {packId} = useParams<{ packId: string }>()
+    const navigateToLearn = (packId: string | undefined) => navigate(`/packs-item/learn/${packId}`)
+    const learnToPack = () => {
+        navigateToLearn(packId)
+    }
     return {
         cardPacks,
         pageCount,
         status,
         cardUserId,
-        isMe
+        isMe,
+        cardName,
+        learnToPack,
+        totalCount,
+        cover
     }
 }
--- src/features/cards/hooks/useFetchCard.ts
+++ src/features/cards/hooks/useFetchCard.ts
@@ -1,20 +1,22 @@
-import {useAppDispatch, useAppSelector} from "../../../app/hooks.ts";
-import {cardPageCount, cardPageParam, cardSortPacks} from "../components/cards/selectors.ts";
-import {useParams} from "react-router-dom";
 import {useEffect} from "react";
+import {useParams} from "react-router-dom";
+import {useAppDispatch, useAppSelector} from "../../../app/hooks.ts";
+import {cardQuestionParams} from "../components/cards/cardSelectors.ts";
 import {getCards, packCardsActions} from "../components/cards/pack-cardSlice.ts";
+import {cardPageCount, cardPageParam, cardSortPacks} from "../components/cards/selectors.ts";
 
 
 export const useFetchCard = () => {
     const pageParams = useAppSelector(cardPageParam)
     const pageCount = useAppSelector(cardPageCount)
     const sortPacks = useAppSelector(cardSortPacks)
+    const searchValue = useAppSelector(cardQuestionParams)
     const dispatch = useAppDispatch()
     const {packId} = useParams<{packId:string}>()
 
     useEffect(() => {
         dispatch(getCards({ cardsPack_id: packId as string }))
-    }, [pageParams, pageCount, sortPacks])
+    }, [pageParams, pageCount, sortPacks, searchValue])
 
     useEffect(() => {
         dispatch(packCardsActions.setQueryParams({ cardsPack_id: packId as string }))
