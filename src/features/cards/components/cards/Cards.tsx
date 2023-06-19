9277af0fb7cee6da769c2a10b73d66395a69853a
Yelkira <penda.fu@gmail.com>
2023-06-19 20:24:10 +0300

    feature/ added NavigateToBack FilterPanelsCards components

--- src/features/cards/components/cards/Cards.tsx
+++ src/features/cards/components/cards/Cards.tsx
@@ -1,18 +1,18 @@
-import {CardsPackTable} from "../cardsPackTable/CardsPackTable.tsx";
-import {ImageCard} from "../ImageWithCard/ImageCard.tsx";
-import {useAppSelector} from "../../../../app/hooks.ts";
+import {NavigateToBack} from "../../../../common/components/NavigateToBack/NavigateToBack.tsx";
 import {useFetchCard} from "../../hooks/useFetchCard.ts";
+import {CardsPackTable} from "../cardsPackTable/CardsPackTable.tsx";
+import {FilterPanelsCards} from "../filterPanels/FilterPanelsCards.tsx";
 import {PanelsCards} from "../panels/PanelsCards.tsx";
 
 export const Cards = () => {
 
-    const cardImage = useAppSelector(state => state.packCardsReducer.packCards.packDeckCover)
 
     useFetchCard()
 
     return (
         <div>
-            <ImageCard deckCover={cardImage} width={'150px'} height={'150px'}/>
+            <NavigateToBack />
+            <FilterPanelsCards/>
             <CardsPackTable/>
             <PanelsCards/>
         </div>
